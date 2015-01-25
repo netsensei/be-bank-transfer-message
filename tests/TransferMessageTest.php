74e37b945bcdc0631874030476b34b9cd6643e9d
Matthias Vandermaesen <matthias@colada.be>
2015-01-25 13:50:38 +0100

    added: unit tests

--- tests/TransferMessageTest.php
+++ tests/TransferMessageTest.php
@@ -8,21 +8,47 @@ use Netsensei\BeBankTransferMessage\Exception\TransferMessageException;
 class TransferMessageTest extends \PHPUnit_Framework_TestCase
 {
 
-    private $transferMessage;
-
-    public function setUp() {
-        $this->transferMessage = new TransferMessage();
+    public function testConstructorNumberUndefined() {
+        $transferMessage = new TransferMessage();
+        $number = $transferMessage->getNumber();
+        $this->assertNotNull($number);
     }
 
-    public function testNumberSetterOutOfLowerBound() {
+    public function testConstructorNumberDefined() {
+        $transferMessage = new TransferMessage(12345);
+        $number = $transferMessage->getNumber();
+        $this->assertNotNull($number);
+    }
+
+    public function testNumberSetterNotAnInt() {
+        $transferMessage = new TransferMessage();
         try {
-            $number = 0;
-            $this->transferMessage->setNumber($number);
+            $number = 'abcd';
+            $transferMessage->setNumber($number);
         } catch (TransferMessageException $e) {
             $previous = $e->getPrevious();
             $this->assertInstanceOf('\InvalidArgumentException', $previous);
             $this->assertEquals(
-                'The number should be larger then 0 and smaller then 9999999999.',
+                'The number should be an integer larger then 0 and smaller then 9999999999.',
+                $previous->getMessage()
+            );
+
+            return;
+        }
+
+        $this->markTestIncomplete('This test should have thrown an exception');
+    }
+
+    public function testNumberSetterOutOfLowerBound() {
+        $transferMessage = new TransferMessage();
+        try {
+            $number = 0;
+            $transferMessage->setNumber($number);
+        } catch (TransferMessageException $e) {
+            $previous = $e->getPrevious();
+            $this->assertInstanceOf('\InvalidArgumentException', $previous);
+            $this->assertEquals(
+                'The number should be an integer larger then 0 and smaller then 9999999999.',
                 $previous->getMessage()
             );
 
@@ -33,16 +59,16 @@ class TransferMessageTest extends \PHPUnit_Framework_TestCase
     }
 
     public function testNumberSetterOutofUpperBound() {
-
+        $transferMessage = new TransferMessage();
         $number = 10000000000;
         try {
             $number = 0;
-            $this->transferMessage->setNumber($number);
+            $transferMessage->setNumber($number);
         } catch (TransferMessageException $e) {
             $previous = $e->getPrevious();
             $this->assertInstanceOf('\InvalidArgumentException', $previous);
             $this->assertEquals(
-                'The number should be larger then 0 and smaller then 9999999999.',
+                'The number should be an integer larger then 0 and smaller then 9999999999.',
                 $previous->getMessage()
             );
 
@@ -50,5 +76,21 @@ class TransferMessageTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->markTestIncomplete('This test should have thrown an exception');
+    }
+
+    public function testCarryGetterNotNull() {
+        $transferMessage = new TransferMessage();
+        $transferMessage->generate();
+        $carry = $transferMessage->getCarry();
+
+        $this->assertNotNull($carry);
+    }
+
+    public function testCarryGetterIsModZeroException() {
+        $transferMessage = new TransferMessage(119698);
+        $transferMessage->generate();
+        $carry = $transferMessage->getCarry();
+
+        $this->assertEquals($transferMessage::MOD_BE_BANK_TRANSFER_MESSAGE, $carry);
     }
 }
